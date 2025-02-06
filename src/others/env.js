cf65b15019bac9b8c842b90176da58a9c37eff94
Okata Ntow <okatantow@gmail.com>
2025-02-06 17:42:01 +0000

    up

--- src/others/env.js
+++ src/others/env.js
@@ -3,8 +3,6 @@
 // export const baseURL = "http://localhost/ato_data_analytics/ato-ushahidi-api/public/index.php/api/";
 // export const baseURLApp = "http://localhost:3000/";
 
-// export const merchantID = "242633039356";
-// export const apiKEY = "RLP1BCxsESqU5hNc1wdVr-BWKJDtabVlr37WxWSBCi-vMZgypQZHjYojRx03bUe-VGqg8Cg1Eu0HqgOs1nA";
 
 // //stage
 // export const baseURL = "https://stingray-app-zwlrd.ondigitalocean.app/api/";
@@ -16,5 +14,3 @@
 export const baseURL = "https://octopus-app-nsawa.ondigitalocean.app/api/";
 export const baseURLApp = "https://pay.admissions.ashesi.edu.gh/";
 
-export const merchantID = "095370042777";
-export const apiKEY = "9V9tZjdFodWI6mj3pwJNP-GZYQNhaOR9ObP5Gl7EKP-6jA2B6LgKzZ2c6Q9zn34-qTwt3mmC7eL2u8oHNai";
