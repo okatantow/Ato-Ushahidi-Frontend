c73ffcd10a5f525b7eb8bb078b617458e82f6f38
Okata Ntow <okatantow@gmail.com>
2025-02-06 17:22:56 +0000

    Update env.js

--- src/others/env.js
+++ src/others/env.js
@@ -1,10 +1,10 @@
 
-// localhost
-export const baseURL = "http://localhost/ato_data_analytics/ato-ushahidi-api/public/index.php/api/";
-export const baseURLApp = "http://localhost:3000/";
+// // localhost
+// export const baseURL = "http://localhost/ato_data_analytics/ato-ushahidi-api/public/index.php/api/";
+// export const baseURLApp = "http://localhost:3000/";
 
-export const merchantID = "242633039356";
-export const apiKEY = "RLP1BCxsESqU5hNc1wdVr-BWKJDtabVlr37WxWSBCi-vMZgypQZHjYojRx03bUe-VGqg8Cg1Eu0HqgOs1nA";
+// export const merchantID = "242633039356";
+// export const apiKEY = "RLP1BCxsESqU5hNc1wdVr-BWKJDtabVlr37WxWSBCi-vMZgypQZHjYojRx03bUe-VGqg8Cg1Eu0HqgOs1nA";
 
 // //stage
 // export const baseURL = "https://stingray-app-zwlrd.ondigitalocean.app/api/";
@@ -12,9 +12,9 @@ export const apiKEY = "RLP1BCxsESqU5hNc1wdVr-BWKJDtabVlr37WxWSBCi-vMZgypQZHjYojR
 // export const merchantID = "242633039356";
 // export const apiKEY = "RLP1BCxsESqU5hNc1wdVr-BWKJDtabVlr37WxWSBCi-vMZgypQZHjYojRx03bUe-VGqg8Cg1Eu0HqgOs1nA";
 
-//  //live
-// export const baseURL = "https://seahorse-app-put3l.ondigitalocean.app/api/";
-// export const baseURLApp = "https://pay.admissions.ashesi.edu.gh/";
+ //live
+export const baseURL = "https://octopus-app-nsawa.ondigitalocean.app/api/";
+export const baseURLApp = "https://pay.admissions.ashesi.edu.gh/";
 
-// export const merchantID = "095370042777";
-// export const apiKEY = "9V9tZjdFodWI6mj3pwJNP-GZYQNhaOR9ObP5Gl7EKP-6jA2B6LgKzZ2c6Q9zn34-qTwt3mmC7eL2u8oHNai";
+export const merchantID = "095370042777";
+export const apiKEY = "9V9tZjdFodWI6mj3pwJNP-GZYQNhaOR9ObP5Gl7EKP-6jA2B6LgKzZ2c6Q9zn34-qTwt3mmC7eL2u8oHNai";
