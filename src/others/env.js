eb64a70d1447997c751becde103d279fdfbf1a31
Okata Ntow <okatantow@gmail.com>
2025-02-06 17:42:19 +0000

    Update env.js

--- src/others/env.js
+++ src/others/env.js
@@ -4,11 +4,7 @@
 // export const baseURLApp = "http://localhost:3000/";
 
 
-// //stage
-// export const baseURL = "https://stingray-app-zwlrd.ondigitalocean.app/api/";
-// export const baseURLApp = "https://dolphin-app-583eh.ondigitalocean.app/";
-// export const merchantID = "242633039356";
-// export const apiKEY = "RLP1BCxsESqU5hNc1wdVr-BWKJDtabVlr37WxWSBCi-vMZgypQZHjYojRx03bUe-VGqg8Cg1Eu0HqgOs1nA";
+
 
  //live
 export const baseURL = "https://octopus-app-nsawa.ondigitalocean.app/api/";
